24f6943bcf977be73762661b122b9f4172426902
yooimran <ahmimranuddin@gmail.com>
2024-10-20 00:06:27 +0600

    updated

---
+++ scriptform.js
@@ -0,0 +1,9 @@
+document.getElementById('show-login').addEventListener('click', function () {
+  document.getElementById('signup-form').classList.add('hidden');
+  document.getElementById('login-form').classList.remove('hidden');
+});
+
+document.getElementById('show-signup').addEventListener('click', function () {
+  document.getElementById('login-form').classList.add('hidden');
+  document.getElementById('signup-form').classList.remove('hidden');
+});
